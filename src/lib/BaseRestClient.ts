89835c9279b72679689632991e105400ddf8145a
JJ-Cro <bryanadams3312@gmail.com>
2025-12-15 18:11:25 +0100

    chore(): remove header

--- src/lib/BaseRestClient.ts
+++ src/lib/BaseRestClient.ts
@@ -161,7 +161,6 @@ export abstract class BaseRestClient {
       headers: {
         'Content-Type': 'application/json',
         'X-Gate-Channel-Id': CHANNEL_ID,
-        'X-Gate-Size-Decimal': '1', // v4.106.0: Ensure the server correctly handles size fields
         locale: 'en-US',
       },
     };
